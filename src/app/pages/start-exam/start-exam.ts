edb321d12770ffae19af145ed9ba9ae373dea6a9
VinhTin-AQUA <tinhovinh@gmail.com>
2025-12-18 22:22:41 +0700

    update timer

--- src/app/pages/start-exam/start-exam.ts
+++ src/app/pages/start-exam/start-exam.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, computed, HostListener, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Question } from '../../core/models/question';
@@ -22,14 +22,34 @@ export class StartExam {
     submitted = false;
     score = 0;
     correctCount = 0;
+    examDuration = signal<number>(10); // phút
+    remainingSeconds = signal<number>(10 * 60);
+    timerInterval: any = null;
+    formattedTime = computed(() => {
+        const sec = this.remainingSeconds();
+        const m = Math.floor(sec / 60);
+        const s = sec % 60;
+        return `${m}:${s.toString().padStart(2, '0')}`;
+    });
+    isStarted = signal<boolean>(false);
 
     constructor(private tauriCommandService: TauriCommandService) {}
 
     async ngOnInit() {
-        await this.RandomQuestions();
+        await this.onRandomQuestions();
+        // window.addEventListener('beforeunload', this.beforeUnloadHandler);
     }
 
-    private async RandomQuestions() {
+    ngOnDestroy() {
+        // window.removeEventListener('beforeunload', this.beforeUnloadHandler);
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+    }
+
+    //=========================================
+
+    async onRandomQuestions() {
         const questions = await this.tauriCommandService.invokeCommand<Question[]>(
             TauriCommandService.GET_RANDOM_QUESTIONS_COMMAND,
             { rand: this.totalQuestionPerExam() }
@@ -58,21 +78,73 @@ export class StartExam {
         this.submitted = false;
         this.score = 0;
         this.correctCount = 0;
+        this.isStarted.set(false);
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+        this.remainingSeconds.set(this.examDuration() * 60);
     }
-
-    //=========================================
 
     onInputTotalQuestionPerExam(num: number) {
         this.totalQuestionPerExam.set(num);
     }
 
-    async onRandomQuestions() {
-        console.log(this.totalQuestionPerExam());
-        await this.RandomQuestions();
+    onChangeExamDuration(value: number) {
+        this.examDuration.set(value);
+        this.remainingSeconds.set(this.examDuration() * 60);
+    }
+
+    checkStartExam(event: Event) {
+        if (this.isStarted() == false) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            this.toastStore.showToastMessage(
+                true,
+                false,
+                'Chưa bắt đầu thi',
+                'Vui lòng bắt đầu bài thi'
+            );
+        }
+    }
+
+    private startTimer() {
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+
+        this.remainingSeconds.set(this.examDuration() * 60);
+
+        this.timerInterval = setInterval(() => {
+            const current = this.remainingSeconds();
+
+            if (current <= 1) {
+                clearInterval(this.timerInterval);
+                this.remainingSeconds.set(0);
+                this.autoSubmit();
+            } else {
+                this.remainingSeconds.set(current - 1);
+            }
+        }, 1000);
+    }
+
+    startExam() {
+        this.submitted = false;
+        this.selectedAnswers = {};
+        this.isStarted.set(true);
+        this.startTimer();
+        this.toastStore.showToastMessage(true, true, 'Bắt đầu', 'Bắt đầu bài thi');
     }
 
     submit() {
+        this.toastStore.showToastMessage(true, true, 'Kết thúc', 'Kết thúc bài thi');
+        this.isStarted.set(false);
         this.submitted = true;
+
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+
         this.correctCount = 0;
 
         for (const q of this.questions()) {
@@ -83,13 +155,13 @@ export class StartExam {
             }
         }
 
-        const totalQuestions = this.questions().length;
+        const total = this.questions().length;
+        this.score = total > 0 ? Math.round((this.correctCount / total) * 10 * 100) / 100 : 0;
+    }
 
-        // Bảo vệ chia cho 0
-        if (totalQuestions > 0) {
-            this.score = Math.round((this.correctCount / totalQuestions) * 10 * 100) / 100;
-        } else {
-            this.score = 0;
+    autoSubmit() {
+        if (!this.submitted) {
+            this.submit();
         }
     }
 }
